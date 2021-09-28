def4b2ad622850360321a27b0879cd3d1a0bb164
Dana <s21lucaseda@orange.k12.nj.us>
2021-09-28 09:29:19 -0400

    new updated

--- Lab - Structures.playground/Pages/3. Exercise - Memberwise and Custom Initializers.xcplaygroundpage/Contents.swift
+++ Lab - Structures.playground/Pages/3. Exercise - Memberwise and Custom Initializers.xcplaygroundpage/Contents.swift
@@ -3,7 +3,10 @@
  
  If you completed the exercise Structs, Instances, and Default Values, you created a `GPS` struct with default values for properties of `latitude` and `longitude`. Create your `GPS` struct again, but this time do not provide default values. Both properties should be of type `Double`.
  */
-
+struct GPS{
+    var latitude: Double
+    
+}
 
 /*:
  Now create a constant instance of `GPS` called `somePlace`, and use the memberwise initializer to set `latitude` to 51.514004, and `longitude` to 0.125226. Print the values of `somePlace`'s properties.
@@ -22,8 +25,26 @@
  
  - Example: If you use the initializer for inches to pass in a height of 65, the initializer should set `heightInInches` to 65 and `heightInCentimeters` to 165.1.
  */
+struct Height{
+    var heightInInches: Double
+    var heightInCentimeters: Double
+    
+    
+init(heightInInches2: Double){
+    heightInInches = heightInInches2
+    heightInCentimeters = 2.54 * heightInInches
+    }
 
 
+init(heightInCentimeters2: Double){
+    heightInInches = heightInCentimeters2
+    heightInCentimeters = heightInCentimeters2
+}
+
+}
+var h = Height(heightInCentimeters2: 165.1)
+
+print(h.heightInInches)
 /*:
  Now create a variable instance of `Height` called `someonesHeight`. Use the initializer for inches to set the height to 65. Print out the property for height in centimeters and verify that it is equal to 165.1.
  */
